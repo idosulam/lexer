a5e0e9fe0aa271f88ffefa05a26a9bf87278db77
idohadoker <idohadoker123@yahoo.com>
2023-03-20 15:23:48 +0200

    graph

--- client/src/components/Graph/Graph.jsx
+++ client/src/components/Graph/Graph.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import Tree from "react-d3-tree";
 import Axios from "axios";
+import { ToastContainer } from "react-toastify";
+import { toast } from "react-toastify";
+
+
 import "./graph.css";
 function Graph(props) {
   const jsonTree = props.tree;
@@ -13,7 +17,11 @@ function Graph(props) {
     { parameter: "", value: "", condition: "" },
   ]);
   const [matchingQuery, setMatchingQuery] = useState([]);
+  const [isChecked, setIsChecked] = useState(false);
 
+  const handleSwitchChange = () => {
+    setIsChecked(!isChecked);
+  };
   const handleAddField = () => {
     setList([...list, { parameter: "", value: "", condition: "" }]);
   };
@@ -66,20 +74,44 @@ function Graph(props) {
       }
 
       sql_query = sql_query.slice(0, -3);
-      promises.push(Axios.get(
-        `http://localhost:3001/api/check_table?sql_query=${sql_query}`
-      ).then( async (response) => {
-        if (response.status === 200) {
-          if(response.data[0] !== undefined)
-          return response.data[0].function_name
-        }
-      }));
+      promises.push(
+        Axios.get(
+          `http://localhost:3001/api/check_table?sql_query=${sql_query}`
+        ).then(async (response) => {
+          if (response.status === 200) {
+            if (response.data[0] !== undefined)
+              return response.data[0].function_name;
+          }
+        })
+      );
     }
     const match = await Promise.all(promises);
     setMatchingQuery(match.filter((item) => item !== undefined));
     console.log(matchingQuery);
-
-
+    if (matchingQuery.length === 0) {
+      toast.error("no functions found", {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      });
+    }
+    else{
+      toast.success(`found ${matchingQuery.length} functions`, {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      });
+    }
   };
 
   function printtree(names, root) {
@@ -122,101 +154,117 @@ function Graph(props) {
     const string = `${nodeDatum.nodeDatum.name} from : ${
       regex.exec(nodeDatum.nodeDatum.file)[0]
     }`;
-    if (matchingQuery.includes(nodeDatum.nodeDatum.name)){
+    if (matchingQuery.includes(nodeDatum.nodeDatum.name)) {
       return (
         <g onClick={() => console.log(nodeDatum)}>
           <circle className="object" fill={color} r="20" />
-          <text fill="black" strokeWidth="1" x="23">
+          <text fill="black" strokeWidth="1" x="30">
             {string}
           </text>
         </g>
       );
-    }
-    else{
-    return (
-      <g onClick={() => console.log(nodeDatum)}>
-        <circle fill={color} r="20" />
-        <text fill="black" strokeWidth="1" x="23">
-          {string}
-        </text>
-      </g>
-    );
+    } else {
+      return (
+        <g onClick={() => console.log(nodeDatum)}>
+          <circle fill={color} r="20" />
+          <text fill="black" strokeWidth="1" x="30">
+            {string}
+          </text>
+        </g>
+      );
     }
   }
 
   return (
     <>
       <h1>Graph</h1>
-      <div style={{ paddingLeft: "100px" }}>
-        {list.map((item, index) => (
-          <div key={index} style={{ paddingBottom: "20px" }}>
-            <div style={{ paddingRight: "20px", display: "inline-block" }}>
-              <select
-                name="parameter"
-                value={item.parameter}
-                onChange={(e) => handleInputChange(e, index)}
-              >
-                <option value="">Choose</option>
-                <option value="if_statements">if statement</option>
-                <option value="while_statements">while statement</option>
-                <option value="return_type">return_type</option>
-                <option value="params">parameters</option>
-                <option value="variables">variable</option>
-              </select>
-            </div>
+      <label style={{ paddingLeft: "100px" }}>
+        <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={handleSwitchChange}
+        />
+        Show search
+      </label>
+      {isChecked && (
+        <div>
+          <div style={{ paddingLeft: "100px" }}>
+            {list.map((item, index) => (
+              <div key={index} style={{ paddingBottom: "20px" }}>
+                <div style={{ paddingRight: "20px", display: "inline-block" }}>
+                  <select
+                    name="parameter"
+                    value={item.parameter}
+                    onChange={(e) => handleInputChange(e, index)}
+                  >
+                    <option value="">Choose</option>
+                    <option value="if_statements">if statement</option>
+                    <option value="while_statements">while statement</option>
+                    <option value="return_type">return_type</option>
+                    <option value="params">parameters</option>
+                    <option value="variables">variable</option>
+                  </select>
+                </div>
 
-            <div style={{ paddingRight: "20px", display: "inline-block" }}>
-              <input
-                className="inputsearch"
-                name="value"
-                value={item.value}
-                onChange={(e) => handleInputChange(e, index)}
-              />
-            </div>
-            {list.length - 1 !== index ? (
-              <div style={{ paddingRight: "20px", display: "inline-block" }}>
-                <select
-                  name="condition"
-                  value={item.condition}
-                  onChange={(e) => handleInputChange(e, index)}
-                >
-                  <option value="">Choose</option>
-                  <option value="not">not</option>
-                  <option value="and">and</option>
-                  <option value="or">or</option>
-                </select>
+                <div style={{ paddingRight: "20px", display: "inline-block" }}>
+                  <input
+                    className="inputsearch"
+                    name="value"
+                    value={item.value}
+                    onChange={(e) => handleInputChange(e, index)}
+                  />
+                </div>
+                {list.length - 1 !== index ? (
+                  <div
+                    style={{ paddingRight: "20px", display: "inline-block" }}
+                  >
+                    <select
+                      name="condition"
+                      value={item.condition}
+                      onChange={(e) => handleInputChange(e, index)}
+                    >
+                      <option value="">Choose</option>
+                      <option value="not">not</option>
+                      <option value="and">and</option>
+                      <option value="or">or</option>
+                    </select>
+                  </div>
+                ) : (
+                  ``
+                )}
+                {list.length !== 1 ? (
+                  <button
+                    onClick={() => handleRemoveField(index)}
+                    className="button-28"
+                  >
+                    Remove
+                  </button>
+                ) : (
+                  ``
+                )}
               </div>
-            ) : (
-              ``
-            )}
-            {list.length !== 1 ? (
-              <button
-                onClick={() => handleRemoveField(index)}
-                className="button-28"
-              >
-                Remove
-              </button>
-            ) : (
-              ``
-            )}
+            ))}
           </div>
-        ))}
-      </div>
-      <button
-        style={{ marginTop: "20px", marginLeft: "100px", marginRight: "20px" }}
-        className="button-28"
-        onClick={handleAddField}
-      >
-        Add
-      </button>
-      <button
-        style={{ marginTop: "20px" }}
-        className="button-28"
-        onClick={handleSubmit}
-      >
-        Submit
-      </button>
-
+          <button
+            style={{
+              marginTop: "20px",
+              marginLeft: "100px",
+              marginRight: "20px",
+            }}
+            className="button-28"
+            onClick={handleAddField}
+          >
+            Add
+          </button>
+          <button
+            style={{ marginTop: "20px" }}
+            className="button-28"
+            onClick={handleSubmit}
+          >
+            Submit
+          </button>
+        </div>
+      )}
       <div>
         <div
           style={{
@@ -239,7 +287,7 @@ function Graph(props) {
             pathClassFunc={pathClassFunc}
             onZoom={handleZoom}
             renderCustomNodeElement={updatecolor}
-            separation={{ siblings: 1.2, nonSiblings: 2 }}
+            separation={{ siblings: 1, nonSiblings: 2 }}
           />
           <style>
             {`
@@ -292,6 +340,18 @@ function Graph(props) {
           }
         `}
         </style>
+        <ToastContainer
+          position="top-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+        />
       </div>
     </>
   );
