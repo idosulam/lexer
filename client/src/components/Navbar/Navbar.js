99b02384ca499d16fb18944e042ca020aaadeec9
idohadoker <idohadoker123@yahoo.com>
2023-04-21 15:00:56 +0300

    added logo

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <nav className="nav">
       <Link to="/" className="site-title">
-      <img className="logo-img" src={require('../../components/Images/icon.png')} alt="Logo" />
+      <img className="logo-img" src={require('../Images/icon')} alt="Logo"/>
       </Link>
       <ul style={{fontFamily: 'jost',outline: '#00FF00 solid 1px'}}>
         <CustomLink to="/projects">Projects</CustomLink>
