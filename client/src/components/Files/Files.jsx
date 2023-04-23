7ecf8f4d35efa417bf1eadd29243f03732bda400
idohadoker <idohadoker123@yahoo.com>
2023-04-23 13:04:30 +0300

    added stuff

--- client/src/components/Files/Files.jsx
+++ client/src/components/Files/Files.jsx
@@ -1,11 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Files.css";
-import {FaArrowAltCircleDown,FaArrowAltCircleRight} from 'react-icons/fa'
+import { FaArrowAltCircleDown, FaArrowAltCircleRight } from "react-icons/fa";
 function Files(props) {
-  const headerFiles = props.files.filter((file) =>
-    file.file_name.endsWith(".h")
-  );
-  const srcFiles = props.files.filter((file) => file.file_name.endsWith(".c"));
+  const [srcFiles, setsrcFiles] = useState([]);
+  const [headerFiles, setheaderFiles] = useState([]);
   const [selectedFile, setSelectedFile] = useState("");
   const [fileContent, setFileContent] = useState("");
   const [headerDropdown, setHeaderDropdown] = useState(false);
@@ -16,6 +14,18 @@ function Files(props) {
     setSelectedFile(file.file_name);
     setFileContent(file.file);
   };
+  useEffect(() => {
+    const headerFilesprops = props.files.filter((file) =>
+      file.file_name.endsWith(".h")
+    );
+    const srcFilesprops = props.files.filter((file) =>
+      file.file_name.endsWith(".c")
+    );
+
+    setheaderFiles(headerFilesprops);
+    setsrcFiles(srcFilesprops);
+  }, [props.files]);
+
 
   const updateFileContentsrc = (index) => {
     const file = srcFiles[index];
@@ -24,7 +34,7 @@ function Files(props) {
   };
 
   return (
-    <div style={{ paddingLeft: "140px", paddingTop: '30px' }}>
+    <div style={{ paddingLeft: "140px", paddingTop: "30px" }}>
       <div className="App" style={{ backgroundColor: "#000" }}>
         <ul className="file-list">
           {headerFiles.length > 0 && (
@@ -34,10 +44,13 @@ function Files(props) {
                 style={{ fontFamily: "jost" }}
               >
                 {headerDropdown ? (
-                  <button><FaArrowAltCircleDown/> includes</button>
-                ):(
-                  <button><FaArrowAltCircleRight/> includes</button>
-
+                  <button>
+                    <FaArrowAltCircleDown /> includes
+                  </button>
+                ) : (
+                  <button>
+                    <FaArrowAltCircleRight /> includes
+                  </button>
                 )}
               </button>
               {headerDropdown && (
@@ -65,12 +78,16 @@ function Files(props) {
                 onClick={() => setsrcdropdown(!srcdropdown)}
                 style={{ fontFamily: "jost" }}
               >
-{srcdropdown ? (
-                  <button><FaArrowAltCircleDown/> src</button>
-                ):(
-                  <button><FaArrowAltCircleRight/> src</button>
-
-                )}              </button>
+                {srcdropdown ? (
+                  <button>
+                    <FaArrowAltCircleDown /> src
+                  </button>
+                ) : (
+                  <button>
+                    <FaArrowAltCircleRight /> src
+                  </button>
+                )}{" "}
+              </button>
               {srcdropdown && (
                 <ul className="dropdown">
                   {srcFiles.map((file, index) => (
@@ -94,12 +111,11 @@ function Files(props) {
         <div
           className="file-content"
           style={{
-            overflow: 'auto',
-            backgroundColor: "#333"
-            
+            overflow: "auto",
+            backgroundColor: "#333",
           }}
         >
-          <h3 style={{color: "#fff",   fontFamily: "jost"}}>{selectedFile}</h3>
+          <h3 style={{ color: "#fff", fontFamily: "jost" }}>{selectedFile}</h3>
           <pre style={{ fontFamily: "jost" }}>
             {fileContent.split("\n").map((line, i) => (
               <div key={i}>
