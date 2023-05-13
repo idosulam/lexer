15bdb7d5394b8da86ea27f5404a8709a6ae14cb8
idohadoker <idohadoker123@yahoo.com>
2023-05-13 21:13:18 +0300

    added promise

--- client/src/components/Pages/Home.js
+++ client/src/components/Pages/Home.js
@@ -12,13 +12,13 @@ function Home() {
   });
 
   function handleRedirect() {
-    /*
+    
     setTimeout(() => {
       const currentUrl = window.location.href;
       const newUrl = currentUrl + "projects";
       window.location.href = newUrl;
-    }, 4000);
-    */
+    }, 2000);
+    
   }
 
   const updateUploadedFiles = (files) =>
@@ -183,97 +183,82 @@ function Home() {
     }
   }
 
-  const upload_files_to_database = async (name, files) => {
-    const json_tree = await sendAPI(files);
-    const data_list = await recv_data();
-    upload_data_list(data_list, name);
-    upload_json_tree(name, json_tree);
-    for (const file of files) {
-      const reader = new FileReader();
-      reader.onload = function () {
-        Axios.post("http://localhost:3001/api/fileinsert", {
-          project_name: name,
-          file: reader.result,
-          file_name: file.name,
-        })
-          .then((response) => {
-            if (response.statusCode === 400) {
-              toast.error("Failed to insert project", {
-                position: "top-center",
-                autoClose: 5000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                theme: "dark",
-              });
-            }
-          })
-          .catch((error) => {
-            toast.error("Failed to insert project", {
-              position: "top-center",
-              autoClose: 5000,
-              hideProgressBar: false,
-              closeOnClick: true,
-              pauseOnHover: true,
-              draggable: true,
-              progress: undefined,
-              theme: "dark",
+
+  const sendToDatabase = async (name, files) => {
+    try {
+      const response = await Axios.post("http://localhost:3001/api/insert", {
+        project_name: name,
+      });
+      if (response.status === 200) {
+        const json_tree = await sendAPI(files);
+        const data_list = await recv_data();
+        await Promise.all([
+          upload_data_list(data_list, name),
+          upload_json_tree(name, json_tree),
+        ]);
+        await Promise.all(
+          files.map(async (file) => {
+            const fileContent = await readFileAsText(file);
+            await Axios.post("http://localhost:3001/api/fileinsert", {
+              project_name: name,
+              file: fileContent,
+              file_name: file.name,
             });
-          });
+          })
+        );
+        handleRedirect();
+        toast.success("Project inserted successfully redirecting...", {
+          position: "top-center",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "dark",
+        });
+      }
+    } catch (error) {
+      if (error.response && error.response.status === 409) {
+        toast.error("Name already exist", {
+          position: "top-center",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "dark",
+        });
+      } else {
+        console.log(error);
+        toast.error("Failed to insert project", {
+          position: "top-center",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "dark",
+        });
+      }
+    }
+  };
+  
+  const readFileAsText = (file) => {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onload = () => {
+        resolve(reader.result);
+      };
+      reader.onerror = (error) => {
+        reject(error);
       };
       reader.readAsText(file);
-    }
-    handleRedirect();
+    });
   };
-
-  const sendToDatabase = (name, files) => {
-    Axios.post("http://localhost:3001/api/insert", {
-      project_name: name,
-    })
-      .then((response) => {
-        if (response.status === 200) {
-          upload_files_to_database(name, files);
-          toast.success("Project inserted successfully redirecting...", {
-            position: "top-center",
-            autoClose: 5000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            theme: "dark",
-          });
-        }
-      })
-      .catch((error) => {
-        if (error.response.status === 409) {
-          toast.error("Name already exist", {
-            position: "top-center",
-            autoClose: 5000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            theme: "dark",
-          });
-        } else {
-          console.log(error);
-          toast.error("Failed to insert project", {
-            position: "top-center",
-            autoClose: 5000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            theme: "dark",
-          });
-        }
-      });
-  };
+  
 
   function handleButtonClick() {
     if (newUserInfo.profileImages.length <= 0) {
